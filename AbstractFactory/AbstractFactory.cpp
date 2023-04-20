4162a798a1cb5070ce84f6177457a16a16ba0c1e
R5-aslevi <124337070+R5-aslevi@users.noreply.github.com>
2023-04-20 21:17:15 +0300

    Update AbstractFactory.cpp

--- AbstractFactory/AbstractFactory.cpp
+++ AbstractFactory/AbstractFactory.cpp
@@ -1,5 +1,6 @@
-// Abstract Factory Design Pattern
-
+// test assaf
+//  Abstract Factory Design Pattern
+//
 // Abstract Factory is a creational design pattern that lets you produce 
 // families of related objects without specifying their concrete classes.
 
